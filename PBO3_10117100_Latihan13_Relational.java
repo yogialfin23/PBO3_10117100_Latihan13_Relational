ffb3b8cca363d84247e04b4504570c33c569836e
yogialfin23 <43507570+yogialfin23@users.noreply.github.com>
2018-09-23 18:47:32 +0700

    Add files via upload

---
+++ PBO3_10117100_Latihan13_Relational.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3_10117100_latihan13_relational;
+
+/**
+ * Yogie Alfin Salim
+ * 10117100
+ * IF-3
+ */
+public class PBO3_10117100_Latihan13_Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bil1 == bil2));
+        System.out.println("a != b = " + (bil1 != bil2));
+        System.out.println("a > b = " + (bil1 > bil2));
+        System.out.println("a < b = " + (bil1 > bil2));
+        System.out.println("b >= a = " + (bil2 >= bil1));
+        System.out.println("b <= a = " + (bil2 <= bil1));
+    }
+    
+}
